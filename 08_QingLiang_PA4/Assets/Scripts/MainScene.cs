3e153390c9da68146f2e1b37ccba95b2182424ee
denniswong10 <48814651+denniswong10@users.noreply.github.com>
2020-04-30 14:51:20 +0800

    Updated by denniswong10

---
+++ 08_QingLiang_PA4/Assets/Scripts/MainScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScene : MonoBehaviour
+{
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ChangeScene", 1);
+    }
+}
